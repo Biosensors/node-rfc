577f09c1eb62480cd271a7d6c906873ac7abc98c
bsrdjan <srdjan.boskovic@sap.com>
2017-11-26 18:05:49 +0100

    unit test

--- test/test.js
+++ test/test.js
@@ -32,6 +32,17 @@ var connParams = {
   lang: 'EN'	
 };
 
+
+var connParams = {
+  user: 'demo',
+  passwd: 'Welcome',
+  ashost:  '10.117.19.101',
+  saprouter: '/H/203.13.155.17/W/xjkb3d/H/172.19.138.120/H/',
+  sysnr: '00',
+  lang: 'EN',
+  client: '100'
+}
+
 describe("Connection", function() {
   var client;
   beforeEach(function(done) {
